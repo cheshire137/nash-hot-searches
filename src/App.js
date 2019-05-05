78c44628568e4c79fe665825f2bcf2ed24f8c2f5
Sarah Vessels <cheshire137@gmail.com>
2019-05-05 10:33:58 -0500

    Drop default styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import MonthlySearchesApi from './models/MonthlySearchesApi';
+import Config from './config.json';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { monthlySearches: [] };
+  }
+
+  componentDidMount() {
+    const api = new MonthlySearchesApi(Config.socrataApi.appToken);
+    api.getMonthlySearches().then(this.onDataLoaded);
+  }
+
+  onDataLoaded = monthlySearches => {
+    console.log(monthlySearches[0]);
+    this.setState(prevState => ({ monthlySearches }));
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
