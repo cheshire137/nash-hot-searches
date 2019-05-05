8b7e664537c2de3f5c6f5e75a7317c24d369eed2
Sarah Vessels <cheshire137@gmail.com>
2019-05-05 10:33:50 -0500

    Add MonthlySearchesApi model

---
+++ src/models/MonthlySearchesApi.js
@@ -0,0 +1,20 @@
+import SocrataApi from './SocrataApi';
+import MonthlySearch from './MonthlySearch';
+
+const apiUrl = 'https://data.nashville.gov/resource/fuaa-r5cm.json';
+
+// https://data.nashville.gov/Public-Services/Nashville-gov-Top-500-Monthly-Searches/fuaa-r5cm
+class MonthlySearchesApi extends SocrataApi {
+  constructor(appToken) {
+    super(appToken, apiUrl);
+  }
+
+  async getMonthlySearches() {
+    const query = '';
+    const dataFormatter = rows => rows.map(row => new MonthlySearch(row));
+    const rows = await this.get(query, dataFormatter);
+    return rows.map(row => new MonthlySearch(row));
+  }
+}
+
+export default MonthlySearchesApi;
