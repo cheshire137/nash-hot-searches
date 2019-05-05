481511f607afdaaedf8267847cad7a5f638517f0
Sarah Vessels <cheshire137@gmail.com>
2019-05-05 10:36:09 -0500

    Start on styling

--- src/App.js
+++ src/App.js
@@ -15,23 +15,35 @@ class App extends Component {
   }
 
   onDataLoaded = monthlySearches => {
-    console.log(monthlySearches[0]);
     this.setState(prevState => ({ monthlySearches }));
   }
 
   render() {
     return (
       <div>
-        <header>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="site-header">
+          <div className="container-lg py-2 d-flex flex-items-center flex-justify-between">
+            <h1
+              className="f3 text-normal"
+            >Nashville.gov Hot Searches</h1>
+          </div>
         </header>
+        <main className="site-main">
+          <div className="container-lg">
+          </div>
+        </main>
+        <footer className="site-footer">
+          <div className="container-lg text-small py-1 d-flex flex-items-center flex-justify-between">
+            <span>
+              Data from <a
+                target="_blank"
+                className="link-gray"
+                rel="noopener noreferrer"
+                href="https://data.nashville.gov/Public-Services/Nashville-gov-Top-500-Monthly-Searches/fuaa-r5cm"
+              >Nashville.gov - Top 500 Monthly Searches</a>
+            </span>
+          </div>
+        </footer>
       </div>
     );
   }
